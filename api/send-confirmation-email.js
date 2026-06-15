7dadcde6e02d8d48f2e945d8e2cbbf19cace3bab
romitoorazio <romitoorazio@gmail.com>
2026-06-15 21:09:58 +0200

    chore: disattiva endpoint conferma temporaneo

--- api/send-confirmation-email.js
+++ api/send-confirmation-email.js
@@ -1,3 +1,3 @@
 export default async function handler(req, res) {
-  return res.status(200).json({ ok: true });
+  return res.status(501).json({ ok: false, message: "Endpoint non ancora attivo." });
 }
