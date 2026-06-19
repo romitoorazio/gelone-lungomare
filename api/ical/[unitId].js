f284869dead33534410a43ce99d37049cfaa07a3
romitoorazio <romitoorazio@gmail.com>
2026-06-19 19:57:16 +0200

    Revert "fix: normalizza unitId anche sulla rotta ical senza estensione"
    This reverts commit 45808f13f5a79db4ad63410e1d1e892b65dbfddc.

--- api/ical/[unitId].js
+++ api/ical/[unitId].js
@@ -1,11 +1,7 @@
 import { sendIcal } from "../_icalExport.js";
 
-function normalizeIcalUnitId(value) {
-  const raw = Array.isArray(value) ? value[0] : value;
-  return String(raw || "").replace(/\.ics$/i, "");
-}
-
 export default async function handler(req, res) {
-  const unitId = normalizeIcalUnitId(req?.query?.unitId);
+  const value = req?.query?.unitId;
+  const unitId = Array.isArray(value) ? value[0] : value;
   return sendIcal(req, res, unitId);
 }
