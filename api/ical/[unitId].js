4e8744fcd203cf062babad13351ce49ce8e2a51f
romitoorazio <romitoorazio@gmail.com>
2026-07-19 11:17:15 +0200

    fix: ripristina compatibilita link iCal con estensione .ics

--- api/ical/[unitId].js
+++ api/ical/[unitId].js
@@ -2,6 +2,11 @@ import { sendIcal } from "../_icalExport.js";
 
 export default async function handler(req, res) {
   const value = req?.query?.unitId;
-  const unitId = Array.isArray(value) ? value[0] : value;
+  const rawUnitId = Array.isArray(value) ? value[0] : value;
+
+  // Compatibilità con i collegamenti già configurati nei portali esterni
+  // (es. /api/ical/lunarossa1.ics) oltre al nuovo URL senza estensione.
+  const unitId = String(rawUnitId || "").replace(/\.ics$/i, "");
+
   return sendIcal(req, res, unitId);
 }
